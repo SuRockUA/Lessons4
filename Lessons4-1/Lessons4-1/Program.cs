f11c1637c6a37bf82e3355200f2a959f855b8bb3
SuRockUA <surockod@gmail.com>
2025-05-18 19:04:39 +0300

    Initial commit

---
+++ Lessons4-1/Lessons4-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lessons4_1;
+
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[51]; 
+        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i * 2;
+        }
+        
+        foreach (int number in numbers)
+        {
+            Console.WriteLine($"Element: {number}");
+        }
+    }
+}
---
+++ Lessons4-4/Lessons4-4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lessons4_4;
+
+class Program
+{
+    static void Main()
+    {
+        char[] alphabet = new char[26];
+        
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            alphabet[i] = (char)('a' + i); 
+        }
+        
+        foreach (char letter in alphabet)
+        {
+            int ascii = (int)letter;
+            Console.WriteLine($"Letter: {letter}, ASCII code: {ascii}");
+        }
+    }
+}
